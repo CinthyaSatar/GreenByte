ad444a8dea336ae01530c9a6d324e591cd6e71a2
Jake Jewell <jakejewell@192.168.219.31>
2025-03-22 19:51:17 -0400

    minor fix

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 from greenbyte import createApp 
 
 app = createApp()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0",  port=8000)
-=======
-from greenbyte import app
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
->>>>>>> b8148221960d1db9d5107eb5bed31e1ac24b19c6
